9a120c733dc95bfd38a62d3b04a64a9f1fb4e71e
AlenAbhilash <alenabhilash2@gmail.com>
2025-04-15 22:35:33 +0530

    Chnages made

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,28 +1,72 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+  import { NgFor, NgIf, NgStyle } from '@angular/common';
+  import { Component,OnInit  } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
+  import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
-})
-export class DashboardComponent {
-  username: string = '';
-  password: string = '';
-  constructor(private router: Router) {
-   const nav = this.router.getCurrentNavigation();
-   const state = nav?.extras.state as { username: string, password: string };
-    if (state) {
-      this.username = state.username;
-      this.password = state.password;
-    } else {
+
+  @Component({
+    selector: 'app-dashboard',
+    standalone: true,
+    imports: [FormsModule,NgIf,NgFor,NgStyle],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss'
+  })
+  export class DashboardComponent implements OnInit {
+    dropdown = false;
+    activeitem: string | null = null;
+    username: string = '';
+    password: string = '';
+    projects: any[] = [];
+    paginationpage: any[] = [];
+    currentPage: number = 1;
+    itemsPerPage: number = 5;
+
+    constructor(private router: Router) {
+      const storedUser = localStorage.getItem('authUser');
+      if (storedUser) {
+        const parsedUser = JSON.parse(storedUser);
+        this.username = parsedUser.name;
+      } else {
+        this.router.navigate(['/']);
+      }
+    }
+    ngOnInit() {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('response not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          this.projects = data;
+          console.log(data)
+          this.updatepage();
+        })
+        .catch(error => {
+          console.error('error', error);
+        });
+    }
+    updatepage() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      this.paginationpage = this.projects.slice(start, end);
+    }    
+    goToPage(page: number) {
+      if (page >= 1 && page <= 3) {  
+        this.currentPage = page;
+        this.updatepage();
+      }
+    }    
+    Dropdown() {
+      this.dropdown = !this.dropdown;
+    }
+    onLogout() {
+      localStorage.removeItem('authUser');
       this.router.navigate(['/']);
     }
+    select(item: string) {
+      this.activeitem = this.activeitem === item ? null : item;
+    }
   }
-  onLogout(){
-    this.router.navigate(['/'])
-
-  }
-}
+  
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,35 +1,48 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 interface userdetails {
   name: string,
   password: string
 }
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private router: Router) { }
+  @ViewChild('loginForm') loginForm!: NgForm;
+  constructor(private router: Router) {}
   user: userdetails = {
     name: '',
     password: ''
-  }
+  };
   onChange() {
-    console.log("username", this.user.name)
-    console.log("password", this.user.password)
+    console.log("username", this.user.name);
+    console.log("password", this.user.password);
   }
   onLogin() {
-    if (this.user.name === '' || this.user.password === '') {
-      alert("Please enter the username and the password");
-    } else {
-      this.router.navigate(['dashboard'], {
-        state: { username: this.user.name, password: this.user.password }
-      })
+    if (this.loginForm.invalid) {
+      Object.values(this.loginForm.controls).forEach(control => control.markAsTouched());
+      return;
+    }
+    const storedUser = localStorage.getItem('authUser');
+    if (!storedUser) {
+      localStorage.setItem('authUser', JSON.stringify(this.user));
+      this.router.navigate(['dashboard']);
+    } else { 
+      const parsedUser = JSON.parse(storedUser);
+      if (parsedUser.name === this.user.name && parsedUser.password === this.user.password) {
+        this.router.navigate(['dashboard']);
+      } else {
+        alert('Invalid username or password');
+      }
     }
   }
+  
 }
